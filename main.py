11ce6ee2dbe7e0caf9e4fe866f2ac7b1e65d28ed
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-04-09 11:50:30 +0530

    adds entry logging

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from execute import Execute
 
 if __name__ == '__main__':
     print('Booting up CEXEC')
+    print('inside main.py before Execute()')
     executor = Execute()
     print('Running CEXEC script')
     exit_code=executor.run()
